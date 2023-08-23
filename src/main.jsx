d5f67d76a51f280ad37efa7c757e28f783999632
Antonio Mortari <113060294+AntonioMortari@users.noreply.github.com>
2023-08-23 16:01:40 -0300

    att

--- src/main.jsx
+++ src/main.jsx
@@ -6,19 +6,26 @@ import GlobalStyle from './styles/global.js'
 
 // theme
 import { ThemeProvider } from 'styled-components'
-import theme from './styles/theme.js'
+import {theme} from './styles/theme.js'
 
 import { Provider } from 'react-redux'
 import store from './store/'
 
+import { ChakraProvider } from '@chakra-ui/react'
+import { themeChakra } from './styles/theme.js'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   
-    <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-          <App />
-      </ThemeProvider>
+    <Provider store={store}> {/*redux*/}
+      <ChakraProvider resetCSS={false} theme={themeChakra}>
+        <ThemeProvider theme={theme}> {/*tema*/}
+          <GlobalStyle /> {/*estilos globais*/}
+
+            <App />
+
+        </ThemeProvider>
+      </ChakraProvider>
     </Provider>
   
 )
--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,5 +1,7 @@
+import {extendTheme} from '@chakra-ui/react'
 
-export default {
+// tema 
+const theme = {
     COLORS:{
         blue:'#0077FE',
         blue_light:'#319AFB',
@@ -15,3 +17,23 @@ export default {
         white_100:'#F6F6F6'
     }
 }
+
+// tema chakra
+const themeChakra = extendTheme({
+    colors:{
+        blue:'#0077FE',
+        blue_light:'#319AFB',
+        blue_hover:'#0065D9',
+        blue_active:'#EAEEFF',
+
+        orange:'#FF9925',
+
+        dark_gray:'#3B3B3B',
+        light_gray:'#565656',
+        
+        white_200:'#EFEFEF',
+        white_100:'#F6F6F6'
+    }
+})
+
+export {theme,themeChakra}
